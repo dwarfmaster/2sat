5fb9c718be9fba46705e539faa11a72d6e43d631
lucas8 <luc.linux@mailoo.org>
2015-10-25 19:05:10 +0100

    Add a few algorithms about inductive graphs

--- tarjan.hs
+++ tarjan.hs
@@ -1,103 +1,86 @@
 -- vim:set foldmethod=marker:
+{-# LANGUAGE ViewPatterns #-}
 
 import Data.Array
 import Data.Maybe
+import Data.Graph.Inductive.Graph
+import Data.Graph.Inductive.Example
 
--- {{{ Pile structure
-data Pile a = EmptyPile | a :< (Pile a)
+-- {{{ Tree structure
+data Tree a = Empty | Branch a [Tree a]
+tshow :: Show a => Tree a -> String
+tshow Empty = "Empty"
+tshow (Branch a l) = "(Branch " ++ show a ++ " " ++ show l ++ ")"
+instance Show a => Show (Tree a) where
+    show = tshow
 
-pile_init :: Pile a
-pile_init = EmptyPile
+tempty :: Tree a -> Bool
+tempty Empty = True
+tempty _     = False
 
-pile_push :: Pile a -> a -> Pile a
-pile_push p e = e :< p
+postOrder :: Tree a -> [a]
+postOrder Empty = []
+postOrder (Branch v ts) = concatMap postOrder ts ++ [v]
 
-pile_pop :: Pile a -> (Maybe a, Pile a)
-pile_pop  EmptyPile = (Nothing, EmptyPile)
-pile_pop (e :< p)   = (Just e, p)
-
-pile_empty :: Pile a -> Bool
-pile_empty EmptyPile = True
-pile_empty _         = False
-
-pile_show :: Show a => Pile a -> String
-pile_show p = "[" ++ pile_show_elem p ++ "]"
- where pile_show_elem :: (Show a) => Pile a -> String
-       pile_show_elem EmptyPile = ""
-       pile_show_elem (e :< EmptyPile) = show e
-       pile_show_elem (e :< p)         = show e ++ ", " ++ pile_show_elem p
-instance Show a => Show (Pile a) where
-    show = pile_show
-
-pile_has :: Eq a => Pile a -> a -> Bool
-pile_has EmptyPile _ = False
-pile_has (h :< t)  e = if h == e then True else pile_has t e
-
-pile_get :: (b -> a -> Bool) -> b -> Pile a -> Maybe a
-pile_get _ _ EmptyPile = Nothing
-pile_get e m (h :< t)  = if e m h then Just h else pile_get e m t
--- }}}
-
--- {{{ Graph structure
--- Graph elements to be build with a tying the knot method
-data EGraph a = EGraph a [EGraph a]
--- Real Graph structure, a list of all nodes
-data Graph a  = Empty
-              | EGraph a :& Graph a
-
--- TODO : make it really cyclic
-build :: Eq a => [a] -> [(a,a)] -> Graph a
-build []    _   = Empty
-build (h:t) lks = ebuild h lks :& build t lks
- where ebuild :: Eq a => a -> [(a,a)] -> EGraph a
-       ebuild e lks = EGraph e lgs
-        where lgs = [ebuild b lks | (a,b) <- lks, a == e]
-
-mapg :: (a -> b) -> Graph a -> Graph b
-mapg _ Empty     = Empty
-mapg f (eg :& g) = mg f eg :& mapg f g
- where mg :: (a -> b) -> EGraph a -> EGraph b
-       mg f (EGraph e sg) = EGraph (f e) $ map (mg f) sg
-
-showg :: Show a => Graph a -> String
-showg Empty = ""
-showg (EGraph e egs :& g) = show e ++ " -> " ++ showegs egs ++ "\n" ++ showg g
- where showegs :: Show a => [EGraph a] -> String
-       showegs lst = foldl fld "[ " lst ++ "]"
-        where fld :: Show a => String -> EGraph a -> String
-              fld str (EGraph e _) = str ++ show e ++ " "
-instance Show a => Show (Graph a) where
-    show = showg
-
-eqegraph :: Eq a => EGraph a -> EGraph a -> Bool
-eqegraph (EGraph e1 _) (EGraph e2 _) = e1 == e2
-instance Eq a => Eq (EGraph a) where
-    (==) = eqegraph
+tmap :: (a -> b) -> Tree a -> Tree b
+tmap _ Empty         = Empty
+tmap f (Branch v ts) = Branch (f v) $ map (tmap f) ts
 
 -- }}}
 
--- {{{ Tarjan algorithm
-tar_mtch :: Eq a => EGraph a -> (EGraph a, Int) -> Bool
-tar_mtch (EGraph e1 _) ((EGraph e2 _),_) = e1 == e2
+-- {{{ Misc function
+-- Return all nodes and their labels
+nds :: Graph gr => gr a b -> [(Int,a)]
+nds (isEmpty -> True)            = []
+nds (matchAny -> ((_,n,l,_), g)) = (n,l) : nds g
 
--- The first argument is the graph
--- The second is the list of seen nodes
--- The third argument is the number for parcours
--- It return the strongly connected components
-tar_iter :: Eq a => Graph a
-                 -> Pile (EGraph a, Int)
-                 -> Int
-                 -> [[a]]
-tar_iter Empty     _ _ = []
-tar_iter (eg :& g) p n = if isJust $ pile_get tar_mtch eg p
-                           then tar_iter g p n
-                           else scc : tar_iter g np nn
- where (scc, np, nn) = parcours eg p n
+-- Get the succesors of a context
+msuc :: Context a b -> [Int]
+msuc (_,_,_,scs) = map snd scs
 
-parcours :: Eq a => EGraph a
-                 -> Pile (EGraph a, Int)
-                 -> Int
-                 -> ([a], Pile (EGraph a, Int), Int)
-parcours (EGraph v sgs) p n = ([], EmptyPile, 0) -- TODO
+-- A special kind of mapping with an accumulator and excluding values
+rd ::    (a -> b -> (b,c)) -- The mapping function, b is the accumulator
+      -> b                 -- The first value of the accumulator
+      -> (c -> Bool)       -- The excluding function : any recognised mapped value is excluded
+      -> [a]               -- The list of values to map on
+      -> ([c],b)           -- The map of values and the la value of the accumulator
+rd _ g e []    = ([],g)
+rd f g e (h:t) = if e r then (rc, rrg) else (r : rc, rrg)
+ where (rg,r)   = f h g
+       (rc,rrg) = rd f rg e t
+
+-- Mapping on graph
+mgmap :: DynGraph gr => (Context a b -> Context c d) -> gr a b -> gr c d
+mgmap _ (isEmpty -> True)   = empty
+mgmap f (matchAny -> (c,g)) = (f c) & mgmap f g
+
+-- Swapping froms and tos of a context
+swap :: Context a b -> Context a b
+swap (f,n,l,t) = (t,n,l,f)
+
+-- Reversing an oriented graph
+mgrev :: DynGraph gr => gr a b -> gr a b
+mgrev = mgmap swap
+-- }}}
+
+-- {{{ Deep-first algorithm
+-- DFS from the node n
+dfs :: Graph gr => Int -> gr a b -> (Tree (Int,a), gr a b)
+dfs n g@(match n -> (Nothing, _)) = (Empty, g)
+dfs n (match n -> (Just c, g))    = (Branch (n,l) ar, rg)
+ where (_,n,l,_) = c
+       rdf i g   = let (rt,rg) = dfs i g in (rg,rt)
+       (ar,rg)   = rd rdf g tempty $ msuc c
+
+-- DFS forest
+dff :: Graph gr => gr a b -> [Tree (Int,a)]
+dff (isEmpty -> True)              = []
+dff g@(matchAny -> ((_,n,_,_), _)) = t : dff rg
+ where (t,rg) = dfs n g
+-- }}}
+
+-- {{{ Topological sorting
+toposort :: Graph gr => gr a b -> [(Int,a)]
+toposort = reverse . concatMap postOrder . dff
 -- }}}
 
